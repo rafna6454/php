c65ae2f8b1bc63168769186fdd8486eed6151000
RafnaFathima <47549860+rafna6454@users.noreply.github.com>
2019-04-09 10:01:47 +0530

    Update get_method.php

--- get_method.php
+++ get_method.php
@@ -7,7 +7,7 @@ exit();
 ?>
 <html>
 <body>
-<form action="<?php $_PHP_SELF?>" action= method="GET">
+<form action="<?php $_PHP_SELF?>" method="GET">
 Name: <input type="text" name="name"/>
 Age: <input type="text" name="age"/>
 <input type="submit"/>
