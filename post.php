7fd55fca6a6c98c31aa486982a6f0b0d830236b1
RafnaFathima <47549860+rafna6454@users.noreply.github.com>
2019-04-09 10:02:28 +0530

    Add files via upload

---
+++ post.php
@@ -0,0 +1,16 @@
+<?php
+if($_POST["name"]||$_POST["age"]) {
+echo "Welcome ".$_POST['name']. "<br/>";
+echo "You are ".$_POST['age']." Years old.";
+exit();
+}
+?>
+<html>
+<body>
+<form action="<?php $_PHP_SELF?>"  method="POST">
+Name: <input type="text" name="name"/>
+Age: <input type="text" name="age"/>
+<input type="submit"/>
+</form>
+</body>
+</html>
